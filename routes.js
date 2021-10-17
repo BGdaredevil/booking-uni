b27086f1660a4eff08c6851c5e6f5161ed9866c3
BGdaredevil <dimityr.dimitr0v@abv.bg>
2021-10-17 13:41:19 +0300

    initial

---
+++ routes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const homeController = require("./src/controllers/homeController.js");
+
+const userController = require("./src/controllers/userController.js");
+// const courseController = require("./src/controllers/courseController.js");
+
+//debug
+// function logger(req, res, next) {
+//   console.log(req.path);
+//   next();
+// }
+// router.use(logger);
+
+router.use("/", homeController);
+router.use("/user", userController);
+// router.use("/course", courseController);
+router.use("*", (req, res) => {
+  console.log("called 404");
+  res.status(404).render("404");
+});
+
+module.exports = router;
